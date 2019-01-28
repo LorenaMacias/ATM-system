9c3603c7b25c38710460234dbefa1e272a8157eb
lorena <36055216+LorenaMacias@users.noreply.github.com>
2019-01-27 19:28:31 -0800

    Add files via upload

--- atm.java
+++ atm.java
@@ -24,10 +24,11 @@ class atm{
 }//end of class atm
 
 class account{
-  String name, AccNum, PinNum;
+  String name, AccNum;
+  short PinNum;
   float balance;
   //my constructor
-  account(String n, float b, String a, String p){
+  account(String n, float b, String a, short p){
     name = n;
     AccNum = a;
     PinNum = p;
@@ -38,10 +39,11 @@ class account{
   void login(){
     System.out.println("Enter your account number: ");
     double accNum = scan.nextDouble();
+    String accnum = String.valueOf(accNum);
     System.out.println("Enter your pin number: ");
     short pinNum = scan.nextShort();
     account a = new account();
-    if(a.check()){
+    if(a.check(accnum, pinNum)){
       System.out.println("Login succesfulk");
     }
   }//end of login function
@@ -73,28 +75,46 @@ class account{
     } // end try/catch/finally
  }//end of create function
 
- bool check(String a, String b){
+ public static boolean check(String accNum, short pinNum){
+   //first we need to read in the file that contains accounts
+   account[] accounts = new account[50];
    try{
    FileReader fr = new FileReader("Accounts.txt");
    BufferedReader br = new BufferedReader(fr);
-
-   String str, name, an, pn;
+   //using most of these variables for constructor
+   String str, name, an;
+   short pn;
    float balance;
-   short count = 1;
+   short count = 1, i = 0;
+   //account[] accounts = new account[50];
    while((str = br.readLine()) != null){
      if(count == 1) {name = str;}
-     if(count == 2) {
+     else if(count == 2) {
        //converts from string to float
        balance = Float.parseFloat(str);
      }
-     if(count == 3) {an = str;}
-     if(count == 4) {pn = str; count = 1;}
+     else if(count == 3) {an = str;}
+     else if(count == 4) {
+     pn = Short.parseShort(str);
+     count = 1;
+     //add person to array
+     accounts[i] = new account(name, balance, an, pn);
+     i++;
+     }
      count++;
    }
    br.close();
    } catch (IOException e){
    out.println("File not found");
   }
+  //now to do the real checking
+  for (int i = 0; i < accounts.length ; i++ ) {
+    //if account found, login successfull
+    if(accounts[i].AccNum == accNum &&
+       accounts[i].PinNum == pinNum){return true;}
+  }
+  //return false cause no match
+  return false;
  }
 }//end of account class
 
